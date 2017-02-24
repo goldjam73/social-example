70e6192da7afbd3fe69753d797d079e52ccf7f6e
jagoldsmith73 <jagoldsmith73@yahoo.com>
2017-02-24 11:54:10 -0500

    Addition of @todos.

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -8,13 +8,13 @@ var ngAnnotate = require('gulp-ng-annotate');
 gulp.task('js', function() {
 	gulp.src(['ng/module.js', 'ng/**/*.js'])
 		.pipe(sourcemaps.init())
-			.pipe(concat('app.js'))
-			.pipe(ngAnnotate())
-	        .pipe(iife({
-	            useStrict: true,
-	            prependSemicolon: false
-	        }))
-			.pipe(uglify())
+		.pipe(concat('app.js'))
+		.pipe(ngAnnotate())
+		.pipe(iife({
+			useStrict: true,
+			prependSemicolon: false
+		}))
+		.pipe(uglify())
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('assets'));
 });
